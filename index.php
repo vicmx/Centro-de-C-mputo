cae9e0ddd70bf116ed2aa7b43700de1495246502
vicmx <vicmx@live.com>
2018-11-08 15:47:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,29 @@ if (mysqli_connect_error()) {
         . mysqli_connect_error());
 }
 
+$sql = "Select Matricula, Nombre, carrera_id  from consulta_alumnos ORDER BY Nombre  desc";
+
+$result = $mysqli->query($sql);
+
+
+
+    /* obtener el array de objetos */
+    while ($fila = $result->fetch_row()) {
+        echo $fila[0] . "<br>" ;
+        echo $fila[1] . "<br>";
+        $sql2 = "Select nombre_carrera from carreras where cid =$fila[2]";
+
+        $result2 = $mysqli->query($sql2);
+
+        $row = $result2->fetch_row();
+
+        echo $row[0] ."<br>";
+    }
+
+    /* liberar el conjunto de resultados */
+$result->close();
+
+
 echo 'Éxito... ' . $mysqli->host_info . "\n";
 
 $mysqli->close();
