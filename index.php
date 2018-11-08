148fa6f7f9d6907f81e2c2f84c2fb0ca37217b4f
vicmx <vicmx@live.com>
2018-11-08 17:55:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
 
 <?php
-$mysqli = new mysqli('localhost', 'root', '122077', 'registros_eesm');
-
-/*
- * Esta es la forma OO "oficial" de hacerlo,
- * AUNQUE $connect_error estaba averiado hasta PHP 5.2.9 y 5.3.0.
- */
-if ($mysqli->connect_error) {
-    die('Error de Conexión (' . $mysqli->connect_errno . ') '
-        . $mysqli->connect_error);
-}
-
-/*
- * Use esto en lugar de $connect_error si necesita asegurarse
- * de la compatibilidad con versiones de PHP anteriores a 5.2.9 y 5.3.0.
- */
-if (mysqli_connect_error()) {
-    die('Error de Conexión (' . mysqli_connect_errno() . ') '
-        . mysqli_connect_error());
-}
+include "cabecera.php";
+global $mysqli;
 
 $sql = "Select Matricula, Nombre, carrera_id  from consulta_alumnos ORDER BY Nombre  desc";
 
@@ -47,5 +30,4 @@ $result->close();
 
 echo 'Éxito... ' . $mysqli->host_info . "\n";
 
-$mysqli->close();
-?>
+include "pie.php";
