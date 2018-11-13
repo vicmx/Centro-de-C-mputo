d964110433efe5cc1acb3ca51c0ddfa819ff7df1
Víctor Manuel <vicmx@live.com>
2018-11-13 16:59:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,13 @@
+<?php
+$pagetitle="Centro de Cómputo";
+
+include "cabecera.php";
+?>
+
+<h1>Registro al Centro de Cómputo</h1>
+<p>
+<a href="lista-tabla-alumnos.php">Ver lista de alumnos</a>
+</p>
 
 <?php
-include "cabecera.php";
-global $mysqli;
-
-$sql = "Select Matricula, Nombre, carrera_id  from consulta_alumnos ORDER BY Nombre  desc";
-
-$result = $mysqli->query($sql);
-
-
-
-    /* obtener el array de objetos */
-    while ($fila = $result->fetch_row()) {
-        echo $fila[0] . "<br>" ;
-        echo $fila[1] . "<br>";
-        $sql2 = "Select nombre_carrera from carreras where cid =$fila[2]";
-
-        $result2 = $mysqli->query($sql2);
-
-        $row = $result2->fetch_row();
-
-        echo $row[0] ."<br>";
-
-        $result2->close();
-    }
-
-    /* liberar el conjunto de resultados */
-$result->close();
-
-
-echo 'Éxito... ' . $mysqli->host_info . "\n";
-
 include "pie.php";
