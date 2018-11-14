157065357cdd64be22baaf791c4fbaba8482da57
Víctor Manuel <vicmx@live.com>
2018-11-13 20:55:32 -0600

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,13 @@
+<?php
+$pagetitle="Centro de Cómputo";
+
+include "cabecera.php";
+?>
+
+<h1>Registro al Centro de Cómputo</h1>
+<p>
+<a href="lista-tabla-alumnos.php">Ver lista de alumnos</a>
+</p>
+
+<?php
+include "pie.php";
