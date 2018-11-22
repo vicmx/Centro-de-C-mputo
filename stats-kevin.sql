6968c53f55581493fb8db683227a6645dfea1096
Freddkross <freddkross@gmail.com>
2018-11-22 14:28:19 -0600

    correcion de tabla
    se actualizo la tabla de contadores

--- stats-kevin.sql
+++ stats-kevin.sql
@@ -3,16 +3,15 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `contadores` (
-  `cid` int(11) NOT NULL AUTO_INCREMENT,
-  `aniodia` varchar(10) NOT NULL,
-  `anio` varchar(5) NOT NULL,
-  `mes` varchar(5) NOT NULL,
+CREATE TABLE `contadores` (
+  `cid` int(11) NOT NULL,
+  `aniodia` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `anio` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mes` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Info` int(1) NOT NULL DEFAULT '0',
   `Admon` int(2) NOT NULL DEFAULT '0',
   `c_p` int(3) NOT NULL DEFAULT '0',
   `MMD` int(4) NOT NULL DEFAULT '0',
-  `Der` int(5) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`cid`),
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='tabla contadores para estadísticas';
-COMMIT;
+  `Der` int(5) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
