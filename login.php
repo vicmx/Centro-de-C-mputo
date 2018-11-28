42c63340d8d12f605d0644a3691246ee59983005
estefania-mejia <44881951+estefania-mejia@users.noreply.github.com>
2018-11-28 12:31:45 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,48 +1,57 @@
-<?php
-include "conexion.php";
- 
- $onlyuser = 'disabled';
- $GLOBALS = ["$onlyuser"];
- ?>
+<style type="text/css">
+.fondo{
+    background-image: url("images/login.jpg");
+}	
+</style>
 
- <?php
-if (isset($_POST['enviar-u'])) {
-	$varpass = "";
-	$consulta = "select nickname, password from usuarios where nickname = '".$_POST['txtnick']."'";
-    $resultado = mysqli_query($conexion,$consulta);
-       	while( $fila = mysqli_fetch_assoc($resultado) ) {
- 		$varpass = $fila['password'];
-	}
- 	if(!empty($resultado)){
- 		if($varpass == $_POST['txtpass']){
- 			session_start();
- 			$_SESSION['usuario']= $_POST['txtnick'];
- 			$onlyuser = "";
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
- 	}else{
- 		echo "Datos incorrectos";
- 	}
- 	}
-}
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/Style.css">
 
- ?>
-<!DOCTYPE html>	
-<html>
-<head>
-	<title></title>
-</head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta charset="UTF-8">
-<body>
-	<form action="formulario_login.php" method="POST" name="formuser">
-		<label>Usuario</label>
-		<input type="text" name="txtnick">
-		<label>Contraseña</label>
-		<input type="password" name="txtpass">
-		<input type="submit" name="enviar-u" value="Ingresar">
-		<input type="submit" name="prueba" <?php echo $onlyuser ?>>
-	</form>
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+    <title>Login | EESM</title> 
+  </head>
+
+<body class="fondo">
+	<div class="col-sm-12 col-md-9 " center>
+        <h1 class="display-4 with ">Login</h1>
+            
+			<form class='mt-5' action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" name="formuser">
+				<div class="col-md-7">
+                <label for="inlineFormInputGroup">Usuario</label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                    <div class="input-group-text"><i class=" fa fa-user"></i></div>
+                    </div>
+                    <input type="text" name="txtnick" class="form-control">
+                </div>
+                </div> 
+
+                <div class="col-md-7">
+                <label for="inlineFormInputGroup">Contraseña</label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                    <div class="input-group-text"><i class="fas fa-unlock"></i></div>
+                    </div>
+                    <input type="password" name="txtpass" class="form-control">
+                </div>
+                </div> 
+
+				<button type="submit" class="btn btn-outline-light" name="enviar-u">Buscar</button>
+
+				
+
+			</form>
+	</div>
+
 </body>
-
-
 </html>
